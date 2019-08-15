2e426ba270467c9d0d79f290e00e224430279bfc
jeff.walls <jeff.walls@tri.global>
2019-08-15 15:49:35 -0700

    wip

--- awesomedir/debian.old/menu.lua
+++ awesomedir/debian.old/menu.lua
@@ -1,6 +1,6 @@
 -- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
 
-local awesome = awesome
+module("debian.menu")
 
 Debian_menu = {}
 
@@ -10,15 +10,16 @@ Debian_menu["Debian_Applications_Accessibility"] = {
 Debian_menu["Debian_Applications_Editors"] = {
 	{"Xedit","xedit"},
 }
+Debian_menu["Debian_Applications_File_Management"] = {
+	{"Baobab","/usr/bin/baobab","/usr/share/pixmaps/baobab.xpm"},
+}
 Debian_menu["Debian_Applications_Graphics"] = {
-	{"dotty","/usr/bin/dotty"},
-	{"lefty","/usr/bin/lefty"},
 	{"X Window Snapshot","xwd | xwud"},
 }
 Debian_menu["Debian_Applications_Network_Communication"] = {
+	{"Remmina","/usr/bin/remmina"},
 	{"Telnet", "x-terminal-emulator -e ".."/usr/bin/telnet.netkit"},
 	{"Xbiff","xbiff"},
-	{"xtightvncviewer","/usr/bin/xtightvncviewer"},
 }
 Debian_menu["Debian_Applications_Network_File_Transfer"] = {
 	{"Transmission BitTorrent Client (GTK)","/usr/bin/transmission-gtk","/usr/share/pixmaps/transmission.xpm"},
@@ -29,7 +30,6 @@ Debian_menu["Debian_Applications_Network_Web_Browsing"] = {
 Debian_menu["Debian_Applications_Network"] = {
 	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
 	{ "File Transfer", Debian_menu["Debian_Applications_Network_File_Transfer"] },
-	{"VNC Viewer","xvnc4viewer"},
 	{ "Web Browsing", Debian_menu["Debian_Applications_Network_Web_Browsing"] },
 }
 Debian_menu["Debian_Applications_Programming"] = {
@@ -40,7 +40,6 @@ Debian_menu["Debian_Applications_Programming"] = {
 Debian_menu["Debian_Applications_Science_Mathematics"] = {
 	{"Bc", "x-terminal-emulator -e ".."/usr/bin/bc"},
 	{"Dc", "x-terminal-emulator -e ".."/usr/bin/dc"},
-	{"Units", "x-terminal-emulator -e ".."units"},
 	{"Xcalc","xcalc"},
 }
 Debian_menu["Debian_Applications_Science"] = {
@@ -52,7 +51,6 @@ Debian_menu["Debian_Applications_Shells"] = {
 	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
 }
 Debian_menu["Debian_Applications_System_Administration"] = {
-	{"Debian Task selector", "x-terminal-emulator -e ".."su-to-root -c tasksel"},
 	{"DSL/PPPoE configuration tool", "x-terminal-emulator -e ".."/usr/sbin/pppoeconf","/usr/share/pixmaps/pppoeconf.xpm"},
 	{"Editres","editres"},
 	{"pppconfig", "x-terminal-emulator -e ".."su-to-root -p root -c /usr/sbin/pppconfig"},
@@ -64,6 +62,9 @@ Debian_menu["Debian_Applications_System_Administration"] = {
 Debian_menu["Debian_Applications_System_Hardware"] = {
 	{"Xvidtune","xvidtune"},
 }
+Debian_menu["Debian_Applications_System_Language_Environment"] = {
+	{"Input Method Configuration", "x-terminal-emulator -e ".."/usr/bin/im-config"},
+}
 Debian_menu["Debian_Applications_System_Monitoring"] = {
 	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
 	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
@@ -71,28 +72,31 @@ Debian_menu["Debian_Applications_System_Monitoring"] = {
 	{"Xev","x-terminal-emulator -e xev"},
 	{"Xload","xload"},
 }
-Debian_menu["Debian_Applications_System_Package_Management"] = {
-	{"Aptitude Package Manager (text)", "x-terminal-emulator -e ".."/usr/bin/aptitude-curses"},
-	{"Synaptic Package Manager","x-terminal-emulator -e synaptic-pkexec","/usr/share/synaptic/pixmaps/synaptic_32x32.xpm"},
-}
 Debian_menu["Debian_Applications_System"] = {
 	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
 	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
+	{ "Language Environment", Debian_menu["Debian_Applications_System_Language_Environment"] },
 	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
-	{ "Package Management", Debian_menu["Debian_Applications_System_Package_Management"] },
+}
+Debian_menu["Debian_Applications_Terminal_Emulators"] = {
+	{"XTerm","xterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+	{"XTerm (Unicode)","uxterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
 }
 Debian_menu["Debian_Applications_Viewers"] = {
+	{"Shotwell","/usr/bin/shotwell"},
 	{"Xditview","xditview"},
 }
 Debian_menu["Debian_Applications"] = {
 	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
 	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
+	{ "File Management", Debian_menu["Debian_Applications_File_Management"] },
 	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
 	{ "Network", Debian_menu["Debian_Applications_Network"] },
 	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
 	{ "Science", Debian_menu["Debian_Applications_Science"] },
 	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
 	{ "System", Debian_menu["Debian_Applications_System"] },
+	{ "Terminal Emulators", Debian_menu["Debian_Applications_Terminal_Emulators"] },
 	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
 }
 Debian_menu["Debian_Games_Toys"] = {
@@ -109,15 +113,8 @@ Debian_menu["Debian_Help"] = {
 	{"Info", "x-terminal-emulator -e ".."info"},
 	{"Xman","xman"},
 }
-Debian_menu["Debian_Window_Managers"] = {
-	{"awesome",function () awesome.exec("/usr/bin/awesome") end,"/usr/share/pixmaps/awesome.xpm"},
-}
 Debian_menu["Debian"] = {
 	{ "Applications", Debian_menu["Debian_Applications"] },
 	{ "Games", Debian_menu["Debian_Games"] },
 	{ "Help", Debian_menu["Debian_Help"] },
-	{ "Window Managers", Debian_menu["Debian_Window_Managers"] },
 }
-
-debian = { menu = { Debian_menu = Debian_menu } }
-return debian
